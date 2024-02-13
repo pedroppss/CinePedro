12c1d90ee16281600ed8d247390195004ceb410d
Pedro Pérez Sánchez <pedro.persan@educa.jcyl.es>
2024-02-13 21:41:37 +0100

    cine

--- app/views/recuperarPassword.php
+++ app/views/recuperarPassword.php
@@ -1,13 +1,13 @@
 <!--esto es para recuperar la contraseña, si te has olvidado la contraseña-->
 <body class="container max-w-screen-2xl mx-auto bg-fond_black">
    <main class="bg-no-repeat bg-personalized h-personalized grid content-around justify-around mt-[103px]" style="background-image: url(../images/recuperarContraseña/fondo-imagenes.png);">
-                <div class="p-24 w-500 h-620 rounded-2xl bg-fond_transp">
+                <div class="p-24 w-500 h-[401px] rounded-2xl bg-fond_transp">
                         <a href="login_register.php?ctl=login"><img class="-ml-20 -mt-10"  src="../images/recuperarContraseña/vector.png" alt="vector"></a>
                     <form action="login_register.php?ctl=recuperarPassword" method="POST">
                         <h1 class="text-3xl font-normal mt-12 text-white font-poppins">Recuperacion de Contraseña</h1>
-                        <input class="w-300 h-12 mt-3 bg-fond_black_2 rounded text-base font-normal font-poppins rounded-lg" type="email" name="gmail" id="gmail" placeholder="Email" value="">
+                        <input class="w-300 h-12 mt-3 p-[21px] bg-fond_black_2 rounded text-base font-normal font-poppins" type="email" name="gmail" id="gmail" placeholder="Email" value="">
                         <br>
-                        <button class="w-300 h-12 mt-7 bg-fond_pink text-2xl font-normal text-white font-poppins" type="submit" name="recuperar" value="recuperar">Recuperar</button>
+                        <button class="w-300 h-12 mt-7 bg-fond_pink text-2xl font-normal text-white font-poppins rounded-lg" type="submit" name="recuperar" value="recuperar">Recuperar</button>
                     </form> 
                 </div>
     </main>
